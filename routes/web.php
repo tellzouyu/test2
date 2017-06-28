d5957ae827147f29c7117205072b1690589960ad
tellzouyu <tellzouyu@gmail.com>
2017-06-28 18:36:08 +0800

    laravel install

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//add Test
+Route::get('/aboooooot', function(){
+	return view('about page');
+});
